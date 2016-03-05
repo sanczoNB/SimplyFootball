8b312d88d5ae39e9a11f6beb2ba7d67fba7dc966
Piotr Piosik <pp.piosik@gmail.com>
2016-03-05 14:04:20 +0100

    Add controler dedicated to players

--- SimlyFooball/Controllers/PlayersController.cs
+++ SimlyFooball/Controllers/PlayersController.cs
@@ -1,117 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using SimlyFooball.Models;
 
 namespace SimlyFooball.Controllers
 {
-    public class TeamsController : Controller
+    public class PlayersController : Controller
     {
-        private readonly FootballDbEntities _db = new FootballDbEntities();
+        private FootballDbEntities db = new FootballDbEntities();
 
-        // GET: Teams
+        // GET: Players
         public ActionResult Index()
         {
-            return View(_db.Team.ToList());
+            return View(db.Player.ToList());
         }
 
-        // GET: Teams/Details/5
+        // GET: Players/Details/5
         public ActionResult Details(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Team team = _db.Team.Find(id);
-            if (team == null)
+            Player player = db.Player.Find(id);
+            if (player == null)
             {
                 return HttpNotFound();
             }
-            return View(team);
+            return View(player);
         }
 
-        // GET: Teams/Create
+        // GET: Players/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Teams/Create
+        // POST: Players/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FullName,NickName,Country,City,FondDate")] Team team)
+        public ActionResult Create([Bind(Include = "Id,FirstName,Surname,BirthDate,Position")] Player player)
         {
             if (ModelState.IsValid)
             {
-                _db.Team.Add(team);
-                _db.SaveChanges();
+                db.Player.Add(player);
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(team);
+            return View(player);
         }
 
-        // GET: Teams/Edit/5
+        // GET: Players/Edit/5
         public ActionResult Edit(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Team team = _db.Team.Find(id);
-            if (team == null)
+            Player player = db.Player.Find(id);
+            if (player == null)
             {
                 return HttpNotFound();
             }
-            return View(team);
+            return View(player);
         }
 
-        // POST: Teams/Edit/5
+        // POST: Players/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FullName,NickName,Country,City,FondDate")] Team team)
+        public ActionResult Edit([Bind(Include = "Id,FirstName,Surname,BirthDate,Position")] Player player)
         {
             if (ModelState.IsValid)
             {
-                _db.Entry(team).State = EntityState.Modified;
-                _db.SaveChanges();
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(team);
+            return View(player);
         }
 
-        // GET: Teams/Delete/5
+        // GET: Players/Delete/5
         public ActionResult Delete(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Team team = _db.Team.Find(id);
-            if (team == null)
+            Player player = db.Player.Find(id);
+            if (player == null)
             {
                 return HttpNotFound();
             }
-            return View(team);
+            return View(player);
         }
 
-        // POST: Teams/Delete/5
+        // POST: Players/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(long id)
         {
-            Team team = _db.Team.Find(id);
-            _db.Team.Remove(team);
-            _db.SaveChanges();
+            Player player = db.Player.Find(id);
+            db.Player.Remove(player);
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
@@ -119,7 +116,7 @@ namespace SimlyFooball.Controllers
         {
             if (disposing)
             {
-                _db.Dispose();
+                db.Dispose();
             }
             base.Dispose(disposing);
         }
