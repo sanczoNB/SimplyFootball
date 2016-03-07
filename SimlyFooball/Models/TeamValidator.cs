6d78754d711b6e479e3947cd27b64b8bc3db5dd2
Piotr Piosik <pp.piosik@gmail.com>
2016-03-07 13:59:53 +0100

    Zapomnialem o nowych plikach

---
+++ SimlyFooball/Models/TeamValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimlyFooball.Models
+{
+
+
+
+  public class TeamValidator
+  {
+    [ScaffoldColumn(false)]
+    public long Id { get; set; }
+
+     [Display(Name = "Pełna nazwa")]
+    public string FullName { get; set; }
+
+    [Display(Name = "Nazwa potoczna")]
+    public string NickName { get; set; }
+
+    [Display(Name = "Kraj")]
+    public string Country { get; set; }
+
+    [Display(Name = "Miasto")]
+    public string City { get; set; }
+
+    [Display(Name = "Rok założenia")]
+    public int FoundYear { get; set; }
+  }
+
+  [MetadataType(typeof(TeamValidator))]
+  public partial class Team
+  {
+     
+  }
+
+}
